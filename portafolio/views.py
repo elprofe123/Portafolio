160249095ff2c02cc9dc69c9fccc44346b48d316
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-06 14:09:47 -0300

    toques finales3

--- portafolio/views.py
+++ portafolio/views.py
@@ -2,19 +2,12 @@ import os
 from django.shortcuts import render #type: ignore
 from .models import Project
 
-# from django.contrib.auth.models import User # type: ignore
-# # Crear superusuario basado en variables de entorno
-# username = os.getenv('ADMIN_USERNAME', 'admin')
-# email = os.getenv('ADMIN_EMAIL', 'admin@example.com')
-# password = os.getenv('ADMIN_PASSWORD', '123')
+from django.contrib.auth.models import User # type: ignore
 
-# if not User.objects.filter(username=username).exists():
-#     User.objects.create_superuser(
-#         username=username, email=email, password=password)
-#     print(f"Superusuario '{username}' creado exitosamente.")
-# if not User.objects.filter(username='admin').exists():
-#     User.objects.create_superuser('admin', 'admin@example.com', '123')
-#     print("Superusuario creado exitosamente")
+# Cambiar la contraseña del usuario 'profe'
+user = User.objects.get(username='profe')
+user.set_password('rodrigo123')
+user.save()
 
 # Create your views here.
 def home(request):

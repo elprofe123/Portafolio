6084f418581874d15cd6e0ee6ddac175667eea79
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-05 23:26:17 -0300

    nuevo commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,15 @@
-from django.urls import path # type: ignore
-from django.conf.urls.static import static # type: ignore
+from django.urls import path  # type: ignore
+from django.conf.urls.static import static  # type: ignore
 from blog import views
-from django_portafolio import settings # type: ignore
+from django_portafolio import settings  # type: ignore
 
 
-app_name='blog' # para referenciar a todas las urls del blog
+app_name = 'blog'  # para referenciar a todas las urls del blog
 
 urlpatterns = [
     path('', views.posts, name='posts'),
-    path('<int:post_id>/',views.post_detail,name='post_detail'),#<int:post_id>
+    path('<int:post_id>/', views.post_detail,
+         name='post_detail'),  # <int:post_id>
 ]
 
 if settings.DEBUG:

00c2c9a78bd1ccbeeb8b038c16a18fcb06a5fa6f
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-06 02:24:20 -0300

    ahi voy otra vez parte dos

--- build.sh
+++ build.sh
@@ -10,3 +10,17 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell << END
+from django.contrib.auth.models import User
+import os
+username = os.getenv('ADMIN_USERNAME', 'admin')
+email = os.getenv('ADMIN_EMAIL', 'admin@example.com')
+password = os.getenv('ADMIN_PASSWORD', '123')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superusuario '{username}' creado exitosamente.")
+else:
+    print("El superusuario ya existe.")
+END

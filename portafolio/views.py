1f7bb7aa8d413e833ba035e56e65eda1cacadcf9
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-06 01:34:46 -0300

    actualizando algo

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render #type: ignore
 from .models import Project
+
+from django.contrib.auth.models import User # type: ignore
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '123')
+    print("Superusuario creado exitosamente")
+
 # Create your views here.
 def home(request):
     projects=Project.objects.all()

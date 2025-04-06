4b5a5d1a8d2fcf3d248a0c10744c3355c24c58ed
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-06 11:43:28 -0300

    actualizando settings again

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -85,11 +85,10 @@ WSGI_APPLICATION = 'django_portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost/postgres',
-            conn_max_age=600)
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600
+    )
 }
 
 

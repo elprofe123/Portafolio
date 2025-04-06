a6bb4fc4b3f8694ed1ef550d8044346965426867
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-06 18:00:53 -0300

    modificando MEDIA_URL

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -168,7 +168,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/public/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

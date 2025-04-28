ab18aa6ac3020aae2dccb214408bd68b3ed3744e
elprofe123 <rgq1998@hotmail.com.ar>
2025-04-28 16:33:11 -0300

    toques finales 3

--- portafolio/views.py
+++ portafolio/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth.models import User # type: ignore
 
 # Código temporal para crear un superusuario
 if not User.objects.filter(username="profe").exists():
-    User.objects.create_superuser("profe","123")
+    User.objects.create_superuser("profe", "profe@example.com", "contraseña_segura")
     print("Superusuario creado: profe")
+
 
 # Create your views here.
 def home(request):
